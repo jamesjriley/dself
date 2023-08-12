296745c220d9337066663cebf255b6032d33d78a
James Riley <expatty@gmail.com>
2023-08-12 12:13:54 +1200

    deploy to test index.html

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ app = Flask(__name__, static_folder='build', static_url_path='')
 
 @app.route('/')
 def index():
-    return "Hello, World!"
-
-@app.route('/<path:path>')
-def serve_static(path):
-    return send_from_directory(app.static_folder, path)
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
