5a15e397fedb47a24caf3fe59a449df1b3b40f3f
James Riley <expatty@gmail.com>
2023-08-12 12:05:54 +1200

    deploy to test hellow world

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_folder='build', static_url_path='')
 
 @app.route('/')
 def index():
-    return send_from_directory(app.static_folder, 'index.html')
+    return "Hello, World!"
 
 @app.route('/<path:path>')
 def serve_static(path):
