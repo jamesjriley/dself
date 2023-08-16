58c9d643ebbbe7b2f9a04f77cce907e4bd8fa3a7
James Riley <expatty@gmail.com>
2023-08-16 18:32:55 +1200

    removed auth section

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 # Inside app/__init__.py
 import os
 from flask import Flask, send_from_directory, current_app
-from app.config.secrets import get_secret
+#from app.config.secrets import get_secret
 from app.auth.oauth import configure_oauth
 from app.auth.oauth import auth_bp
 
@@ -9,15 +9,15 @@ def create_app():
     app = Flask(__name__, static_folder='../build', static_url_path='')
 
     # Retrieve secrets using the get_secret function
-    google_client_id = get_secret('dself-395100', 'google-client-id')
-    google_client_secret = get_secret('dself-395100', 'google-client-secret')
-    flask_secret_key = get_secret('dself-395100', 'flask-secret-key') # Retrieve Flask secret key
+    #google_client_id = get_secret('dself-395100', 'google-client-id')
+    #google_client_secret = get_secret('dself-395100', 'google-client-secret')
+    #flask_secret_key = get_secret('dself-395100', 'flask-secret-key') # Retrieve Flask secret key
 
     # Set the secret key for the Flask session
-    app.secret_key = flask_secret_key
+    #app.secret_key = flask_secret_key
 
     # Configure OAuth using the retrieved client ID and secret
-    google_oauth = configure_oauth(app, google_client_id, google_client_secret)
+    #google_oauth = configure_oauth(app, google_client_id, google_client_secret)
     # Register the authentication Blueprint
     app.register_blueprint(auth_bp)
 
