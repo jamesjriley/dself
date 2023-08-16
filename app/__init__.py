b9deab8589332b1e218a50f65719e07e253db2f9
James Riley <expatty@gmail.com>
2023-08-16 19:01:41 +1200

    added auth section back in. included appengine-python-standard in reqs

--- app/__init__.py
+++ app/__init__.py
@@ -9,15 +9,15 @@ def create_app():
     app = Flask(__name__, static_folder='../build', static_url_path='')
 
     # Retrieve secrets using the get_secret function
-    #google_client_id = get_secret('dself-395100', 'google-client-id')
-    #google_client_secret = get_secret('dself-395100', 'google-client-secret')
+    google_client_id = get_secret('dself-395100', 'google-client-id')
+    google_client_secret = get_secret('dself-395100', 'google-client-secret')
     #flask_secret_key = get_secret('dself-395100', 'flask-secret-key') # Retrieve Flask secret key
 
     # Set the secret key for the Flask session
-    #app.secret_key = flask_secret_key
+    app.secret_key = flask_secret_key
 
     # Configure OAuth using the retrieved client ID and secret
-    #google_oauth = configure_oauth(app, google_client_id, google_client_secret)
+    google_oauth = configure_oauth(app, google_client_id, google_client_secret)
     # Register the authentication Blueprint
     app.register_blueprint(auth_bp)
 
